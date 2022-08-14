a40627c4c16fe5cd1b829de7022454b9d2eed6dc
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-14 21:20:24 +0800

    added sticker command

---
+++ src/commands/sticker.ts
@@ -0,0 +1,31 @@
+import { downloadMediaMessage } from "@adiwajshing/baileys";
+import Sticker, { StickerTypes } from "wa-sticker-formatter/dist";
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { IArgs } from "../core/MessageHandler";
+
+export default class extends BaseCommand {
+	name = "sticker";
+	public override execute = async (M: Message, args: IArgs): Promise<any> => {
+		if (M.type !== "imageMessage")
+			return M.reply("This command only works on images");
+
+		const buffer = (await downloadMediaMessage(
+			M.message,
+			"buffer",
+			{}
+		)) as Buffer;
+
+		const author = args.args[0] || "";
+		const packName = args.args[1] || "";
+
+		const sticker = new Sticker(buffer)
+			.setPack(packName)
+			.setAuthor(author)
+			.setType(StickerTypes.FULL)
+			.setQuality(70);
+
+		const stickerBUffer = await sticker.toBuffer();
+		return M.reply(stickerBUffer, "sticker");
+	};
+}
