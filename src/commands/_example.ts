905e9f7dd3fe2eda11bf7f4aa863a06c1257c6d2
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-14 21:20:07 +0800

    added example command

---
+++ src/commands/_example.ts
@@ -0,0 +1,10 @@
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { IArgs } from "../core/MessageHandler";
+
+export default class extends BaseCommand {
+	name = "ping";
+	public override execute = async (M: Message, args: IArgs): Promise<any> => {
+		M.reply("Pong!");
+	};
+}
