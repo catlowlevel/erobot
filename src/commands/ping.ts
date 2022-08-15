5f70ad2e496a4285262f90cae06ac8c43120130d
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-15 19:14:39 +0800

    send button & listMessage

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,3 +1,4 @@
+import { proto } from "@adiwajshing/baileys";
 import { Message } from "../core";
 import { BaseCommand } from "../core/BaseCommand";
 import { IArgs } from "../core/MessageHandler";
@@ -5,6 +6,53 @@ import { IArgs } from "../core/MessageHandler";
 export default class extends BaseCommand {
 	name = "ping";
 	public override execute = async (M: Message, args: IArgs): Promise<any> => {
+		if (args.args[0].startsWith("b")) {
+			return this.client.sendMessage(
+				M.from,
+				{
+					text: "Pong!",
+					buttons: [
+						{
+							buttonId: ".ping b",
+							buttonText: { displayText: "ping" },
+							type: 0,
+						},
+					],
+				},
+				{ quoted: M.message }
+			);
+		}
+
+		if (args.args[0].startsWith("l")) {
+			const sections: proto.Message.ListMessage.ISection[] = [
+				{
+					title: "Ping",
+					rows: [
+						{
+							title: "Ping button",
+							rowId: ".ping b",
+						},
+						{
+							title: "Ping list",
+							rowId: ".ping l",
+						},
+					],
+				},
+			];
+			const count = Number(args.args[1]);
+			for (let i = 0; i < count; i++) {
+				sections[0].rows?.push({ title: `Ping ${i}`, rowId: ".ping -b" });
+			}
+			return this.client.sendMessage(
+				M.from,
+				{
+					text: "Pong!",
+					buttonText: "Required, text on the button to view the list",
+					sections,
+				},
+				{ quoted: M.message }
+			);
+		}
 		M.reply("Pong!");
 	};
 }
