f8c55deb3791e3686b01dc1232a21f7cbaf17e5a
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-15 13:53:55 +0800

    add price command (WIP)

---
+++ src/commands/price.ts
@@ -0,0 +1,22 @@
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { IArgs } from "../core/MessageHandler";
+
+export default class extends BaseCommand {
+	name = "price";
+	public override execute = async (M: Message, args: IArgs): Promise<any> => {
+		const binance = this.client.binanceClient;
+		const symbols = await binance.getFuturesSymbols();
+		const input = args.args[0] || "";
+		if (!!!input) {
+			return M.reply(`Symbol name required!`);
+		}
+		const symbol = symbols.find((s) =>
+			s.toLocaleLowerCase().startsWith(input.toLocaleLowerCase())
+		);
+		if (!symbol) return M.reply("Invalid symbol");
+		const symbolData = await binance.getSymbolData(symbol);
+		const msg = `${symbol} - $${symbolData.currentPrice}`;
+		M.reply(msg);
+	};
+}
