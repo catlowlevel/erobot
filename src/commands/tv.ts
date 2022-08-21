583621fb7586de06c03154972f78a3b967ce4bdc
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-21 11:43:06 +0800

    add tv & tvm command

---
+++ src/commands/tv.ts
@@ -0,0 +1,39 @@
+import { getChartImg, validIndicator, validTf } from "../api/chartimg/api";
+import { searchSymbol } from "../api/tradingview/api";
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { IArgs } from "../core/MessageHandler";
+
+export default class extends BaseCommand {
+	name = "tv";
+	public override execute = async (M: Message, args: IArgs): Promise<any> => {
+		if (args.args.length <= 0) return M.reply("Invalid arguments!");
+
+		let symbol: string | undefined;
+		let tf = "1h";
+		let indicators: string[] = [];
+		for (const arg of args.args) {
+			console.log("arg :>> ", arg);
+			if (!symbol) {
+				const data = await searchSymbol(arg, "BINANCE");
+				if (data.length > 0) {
+					console.log("data[0] :>> ", data[0].description);
+					symbol = data[0].symbol;
+					continue;
+				}
+			}
+			if (validTf(arg)) {
+				tf = arg;
+				continue;
+			}
+			if (validIndicator(arg)) {
+				indicators.push(arg);
+				continue;
+			}
+		}
+
+		if (!symbol) return M.reply("Invalid symbol!");
+		const buffer = await getChartImg(symbol, tf, indicators);
+		return M.reply(buffer, "image");
+	};
+}
---
+++ src/commands/tvm.ts
@@ -0,0 +1,39 @@
+import {
+	getChartImg,
+	getMinitChartImg as getMiniChartImg,
+	validIndicator,
+	validTf,
+} from "../api/chartimg/api";
+import { searchSymbol } from "../api/tradingview/api";
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { IArgs } from "../core/MessageHandler";
+
+export default class extends BaseCommand {
+	name = "tvm";
+	public override execute = async (M: Message, args: IArgs): Promise<any> => {
+		if (args.args.length <= 0) return M.reply("Invalid arguments!");
+
+		let symbol: string | undefined;
+		let tf = "1h";
+		for (const arg of args.args) {
+			console.log("arg :>> ", arg);
+			if (!symbol) {
+				const data = await searchSymbol(arg, "BINANCE");
+				if (data.length > 0) {
+					console.log("data[0] :>> ", data[0].description);
+					symbol = data[0].symbol;
+					continue;
+				}
+			}
+			if (validTf(arg)) {
+				tf = arg;
+				continue;
+			}
+		}
+
+		if (!symbol) return M.reply("Invalid symbol!");
+		const buffer = await getMiniChartImg(symbol);
+		return M.reply(buffer, "image");
+	};
+}
