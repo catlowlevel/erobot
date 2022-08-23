a93cd482586c5c26e72bd8e6b1fdf9d9cfa73584
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-23 11:21:53 +0800

    add eval command

---
+++ src/commands/eval.ts
@@ -0,0 +1,20 @@
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { IArgs } from "../core/MessageHandler";
+
+export default class extends BaseCommand {
+	name = "eval";
+	public override execute = async (
+		M: Message,
+		{ context }: IArgs
+	): Promise<any> => {
+		let out!: string;
+		try {
+			const result = eval(context);
+			out = JSON.stringify(result, null, "\t") || "Evaluated JavaScript";
+		} catch (error) {
+			out = (error as any).message;
+		}
+		return M.reply(out);
+	};
+}
