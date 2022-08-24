f5e856f1c5fea90df586cebc59489af3c0eaa24b
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-24 14:37:10 +0800

    wrap Low

---
+++ src/core/LowDB.ts
@@ -0,0 +1,22 @@
+import { JSONFile, Low } from "@commonify/lowdb";
+
+export class LowDB<T> implements Low<T> {
+    data!: T;
+    write!: Low<T>["write"];
+    read!: Low<T>["read"];
+    adapter!: Low<T>["adapter"];
+    private low: Low<T>;
+    constructor(filename: string, initialIfNull: T, onRead: (data: T) => void) {
+        this.low = new Low(new JSONFile<T>(filename));
+        this["data"] = this.low["data"] as T;
+        this["adapter"] = this.low["adapter"];
+        this["write"] = this.low["write"];
+        this["read"] = this.low["read"];
+
+        this.write().then(async () => {
+            await this.read();
+            this.data ||= initialIfNull;
+            onRead(this.data);
+        });
+    }
+}
