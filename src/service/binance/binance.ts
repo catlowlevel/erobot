76f6d7959536be1e7704702953d5091d5ceda875
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-27 15:07:29 +0800

    export Interval type

--- src/service/binance/binance.ts
+++ src/service/binance/binance.ts
@@ -11,7 +11,7 @@ import { Client } from "../../core";
 import { LowDB } from "../../core/LowDB";
 import { getPercentageChange } from "../../helper/utils";
 // prettier-ignore
-type Interval = "1m" | "3m" | "5m" | "15m" | "30m" | "1h" | "2h" | "4h" | "6h" | "8h" | "12h" | "1d" | "3d" | "1w" | "1M";
+export type Interval = "1m" | "3m" | "5m" | "15m" | "30m" | "1h" | "2h" | "4h" | "6h" | "8h" | "12h" | "1d" | "3d" | "1w" | "1M";
 
 interface Candle {
     open: number;
