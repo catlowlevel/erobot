9c9db19998b41d33c986fcef53ccfcee305907b3
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-27 09:00:18 +0800

    add shorten command

---
+++ src/commands/bitly.ts
@@ -0,0 +1,18 @@
+import { shortenUrl } from "../api/bitly/api";
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { Command } from "../core/Command";
+import { IArgs } from "../core/MessageHandler";
+
+@Command("bitly", {
+    description: "Shorten url",
+    usage: "bitly [url]",
+})
+export default class extends BaseCommand {
+    public override execute = async (M: Message, args: IArgs): Promise<any> => {
+        if (args.args.length <= 0) return M.reply("No url provided!");
+        const url = args.args[0];
+        const shorten = await shortenUrl(url);
+        return M.reply(shorten);
+    };
+}
