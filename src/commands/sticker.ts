31266bd74d709067b6cec39c8b6cf532d2f349f2
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-29 21:03:33 +0800

    handle replied image

--- src/commands/sticker.ts
+++ src/commands/sticker.ts
@@ -1,4 +1,4 @@
-import { downloadMediaMessage } from "@adiwajshing/baileys";
+import { downloadMediaMessage, proto } from "@adiwajshing/baileys";
 import Sticker, { StickerTypes } from "wa-sticker-formatter/dist";
 import { Message } from "../core";
 import { BaseCommand } from "../core/BaseCommand";
@@ -12,6 +12,29 @@ import { IArgs } from "../core/MessageHandler";
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message, args: IArgs): Promise<any> => {
+        console.log(JSON.stringify(M.quoted, null, 2));
+        if (M.quoted) {
+            const messageKeys = Object.keys(M.quoted.message);
+            console.log("messageKeys", messageKeys);
+            //@ts-ignore
+            if (M.quoted.message[messageKeys[0]]?.mimetype?.startsWith("image/")) {
+                const buffer = await M.downloadMediaMessage(M.quoted.message);
+
+                const author = args.args[0] || "";
+                const packName = args.args[1] || "";
+
+                const sticker = new Sticker(buffer)
+                    .setPack(packName)
+                    .setAuthor(author)
+                    .setType(StickerTypes.FULL)
+                    .setQuality(70);
+
+                const stickerBUffer = await sticker.toBuffer();
+                return M.reply(stickerBUffer, "sticker");
+            } else {
+                return M.reply("This command only works on images");
+            }
+        }
         if (M.type !== "imageMessage") return M.reply("This command only works on images");
 
         const buffer = (await downloadMediaMessage(M.message, "buffer", {})) as Buffer;
