544e6d1c7c476ae7e2e9a6c2d50e9a243b6d9be8
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-31 09:43:41 +0800

    register id_ID locale for timeago

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import path from "path";
+import { register } from "timeago.js";
+import id_ID from "timeago.js/lib/lang/id_ID";
 import { Client } from "./core";
 import { MessageHandler } from "./core/MessageHandler";
 export const ROOT_DIR = path.resolve(__dirname, "..");
-
+register("id_ID", id_ID);
 (async () => {
     const client = new Client();
     await client.start();
