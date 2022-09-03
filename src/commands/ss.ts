3ccc30af8fb80689a625ed475a7c3cced94ef6e2
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-09-03 08:23:01 +0800

    add ss command

---
+++ src/commands/ss.ts
@@ -0,0 +1,38 @@
+import getUrls from "get-urls";
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { Command } from "../core/Command";
+import { IArgs } from "../core/MessageHandler";
+import { ss } from "../lib/puppeteer";
+
+@Command("ss", {
+    description: "",
+    usage: "",
+})
+export default class extends BaseCommand {
+    public override execute = async (M: Message, args: IArgs): Promise<any> => {
+        const urls = getUrls(args.context);
+        if (urls.size <= 0) return M.reply("Url required!");
+        let width = 1200;
+        let height = 800;
+        for (const arg of args.args) {
+            if (arg.startsWith("w:")) {
+                try {
+                    const value = arg.split(":")[1];
+                    width = Number(value);
+                } catch {}
+            }
+
+            if (arg.startsWith("h:")) {
+                try {
+                    const value = arg.split(":")[1];
+                    height = Number(value);
+                } catch {}
+            }
+        }
+        const url = urls.values().next().value;
+        console.log("url", url);
+        const buffer = await ss(url, width, height);
+        return M.reply(buffer, "image");
+    };
+}
