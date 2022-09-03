e7fb05b4addc6efced779e6027c98a3f2268c2e2
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-09-03 11:55:43 +0800

    round sticker

--- src/commands/sticker.ts
+++ src/commands/sticker.ts
@@ -4,6 +4,7 @@ import { Message } from "../core";
 import { BaseCommand } from "../core/BaseCommand";
 import { Command } from "../core/Command";
 import { IArgs } from "../core/MessageHandler";
+import { toRoundedImage } from "../lib/canvas";
 
 @Command("sticker", {
     description: "Mengubah gambar menjadi sticker",
@@ -12,32 +13,52 @@ import { IArgs } from "../core/MessageHandler";
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message, args: IArgs): Promise<any> => {
-        console.log(JSON.stringify(M.quoted, null, 2));
-        if (M.quoted) {
+        // console.log(JSON.stringify(M.quoted, null, 2));
+        let buffer: Buffer | undefined;
+        // if (M.quoted) {
+        //     const messageKeys = Object.keys(M.quoted.message);
+        //     console.log("messageKeys", messageKeys);
+        //     //@ts-ignore
+        //     if (M.quoted.message[messageKeys[0]]?.mimetype?.startsWith("image/")) {
+        //         const buffer = await M.downloadMediaMessage(M.quoted.message);
+
+        //         const author = args.args[0] || "";
+        //         const packName = args.args[1] || "";
+
+        //         const sticker = new Sticker(buffer)
+        //             .setPack(packName)
+        //             .setAuthor(author)
+        //             .setType(StickerTypes.FULL)
+        //             .setQuality(70);
+
+        //         const stickerBUffer = await sticker.toBuffer();
+        //         return M.reply(stickerBUffer, "sticker");
+        //     } else {
+        //         return M.reply("This command only works on images");
+        //     }
+        // }
+        if (M.type === "imageMessage") {
+            buffer = (await downloadMediaMessage(M.message, "buffer", {})) as Buffer;
+        } else if (M.quoted) {
             const messageKeys = Object.keys(M.quoted.message);
             console.log("messageKeys", messageKeys);
             //@ts-ignore
             if (M.quoted.message[messageKeys[0]]?.mimetype?.startsWith("image/")) {
-                const buffer = await M.downloadMediaMessage(M.quoted.message);
+                buffer = await M.downloadMediaMessage(M.quoted.message);
+            } else return M.reply("This command only works on images");
+        } else return M.reply("This command only works on images");
 
-                const author = args.args[0] || "";
-                const packName = args.args[1] || "";
+        if (!buffer) return M.reply("Gagal mengunduh gambar!");
 
-                const sticker = new Sticker(buffer)
-                    .setPack(packName)
-                    .setAuthor(author)
-                    .setType(StickerTypes.FULL)
-                    .setQuality(70);
-
-                const stickerBUffer = await sticker.toBuffer();
-                return M.reply(stickerBUffer, "sticker");
-            } else {
-                return M.reply("This command only works on images");
+        if (args.context.startsWith("r:")) {
+            try {
+                const value = args.context.split(":")[1] ?? "20";
+                const radius = Number(value);
+                buffer = await toRoundedImage(buffer, radius);
+            } catch (error) {
+                console.error(error);
             }
         }
-        if (M.type !== "imageMessage") return M.reply("This command only works on images");
-
-        const buffer = (await downloadMediaMessage(M.message, "buffer", {})) as Buffer;
 
         const author = args.args[0] || "";
         const packName = args.args[1] || "";
