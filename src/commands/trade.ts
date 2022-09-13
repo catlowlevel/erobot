d1ff8363c7f41433e03bf48dd3d061aa6bcb2517
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-09-13 11:14:25 +0800

    create order/open trade

--- src/commands/trade.ts
+++ src/commands/trade.ts
@@ -1,8 +1,8 @@
+import { format } from "timeago.js";
 import { Message } from "../core";
 import { BaseCommand } from "../core/BaseCommand";
 import { Command } from "../core/Command";
 import { IArgs } from "../core/MessageHandler";
-import { countDecimalPlaces, getPercentageChange } from "../helper/utils";
 
 @Command("trade", {
     description: "",
@@ -10,68 +10,48 @@ import { countDecimalPlaces, getPercentageChange } from "../helper/utils";
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message, args: IArgs): Promise<any> => {
-        const binance = this.client.binance;
-        const trades = binance.dbTrade.data;
-        let symbols = trades.map((t) => t.symbol);
-        console.log(symbols.length);
-        symbols = [...new Set(symbols)];
-        console.log(symbols.length);
+        //buttonId: `.trade --symbol=${data.symbol} --direction=${direction} --entry=${entries[0]} --sl=${sl}`,
+        const symbol = this.getFlag(args.args, "--symbol");
+        const direction = this.getFlag(args.args, "--direction", ["LONG", "SHORT"]);
+        const size_f = this.getFlag(args.args, "--size");
+        const entry_f = this.getFlag(args.args, "--entry");
+        const sl_f = this.getFlag(args.args, "--sl");
+        const timestamp_f = this.getFlag(args.args, "--timestamp");
+        if (!symbol) return M.reply("Invalid symbol!");
+        if (!direction) return M.reply("Invalid direction!");
+        if (!size_f) return M.reply("Invalid size!");
+        if (!entry_f) return M.reply("Invalid entry!");
+        if (!sl_f) return M.reply("Invalid sl!");
+        if (!timestamp_f) return M.reply("Invalid timestamp!");
 
-        const candles = await binance.getCandles(symbols, "1m", 1, true);
-        const data = trades
-            .filter((t) => {
-                const current = candles.find((c) => c.symbol === t.symbol);
-                if (!current) return false;
-                const jid = t.msg?.key.remoteJid!;
-                if (jid !== M.from) return false;
-                if (t.sl.hit) return false;
-                if (!current) {
-                    console.log("No " + t.symbol);
-                    return false;
-                }
-                const hitTp = t.tp.some((a) => a.hit);
-                if (!hitTp) return false;
-                const hitEntry = t.entries.some((a) => a.hit);
-                if (!hitEntry) return false;
-                return true;
-            })
-            .map((t) => {
-                const current = candles.find((c) => c.symbol === t.symbol)!;
-                const currentPrice = current.candles[current.candles.length - 1].close;
-                const precision = countDecimalPlaces(currentPrice);
-                const hitEntry = t.entries.some((a) => a.hit);
-                const entries = t.entries.filter((a) => a.hit);
-                const entryPrice = hitEntry
-                    ? entries.reduce((acc, curr) => acc + curr.price, 0) / entries.length
-                    : t.entry
-                    ? t.entry
-                    : t.entries[0].price;
-                let percentGap = getPercentageChange(currentPrice, entryPrice);
-                const isMines = currentPrice < entryPrice;
-                percentGap = isMines ? percentGap * -1 : percentGap;
-                return {
-                    currentPrice,
-                    symbol: t.symbol,
-                    entryPrice: Number(entryPrice.toFixed(precision)),
-                    percentGap,
-                    done: !t.tp.some((a) => a.hit === false),
-                };
-            });
+        const size = Number(size_f);
+        const entry = Number(entry_f);
+        const sl = Number(sl_f);
+        const timestamp = Number(timestamp_f);
+        const symbolData = this.client.binance.symbolData[symbol];
+        let quoteOrderQty = size / entry;
+        quoteOrderQty = Number(quoteOrderQty.toFixed(symbolData.quantityPrecision));
+        const cost = (entry * quoteOrderQty) / symbolData.leverage;
+        console.log(
+            JSON.stringify(
+                {
+                    size,
+                    entry,
+                    sl,
+                    quoteOrderQty,
+                    cost,
+                    symbolData,
+                },
+                null,
+                2
+            )
+        );
+        const result = await this.client.binance.createLimitOrder(symbol, "BUY", entry, quoteOrderQty, sl);
 
-        const running = data.filter((d) => !d.done);
+        if (result.limit) await M.reply("Limit order placed!");
+        if (result.tp) await M.reply("Tp order placed!");
+        if (result.sl) await M.reply("Sl order placed!");
 
-        let text = "=====| Running Trade |=====\nSymbol | Price | Current | Gain/Loss\n";
-        running
-            .sort((a, b) => (a.percentGap < b.percentGap ? 1 : -1))
-            .forEach(({ currentPrice, entryPrice, percentGap, symbol }) => {
-                text += `${symbol} | $${entryPrice} | $${currentPrice} | ${percentGap.toFixed(2)}%\n`;
-            });
-        text += "\n=====| Finished Trade |=====\nSymbol | Price | Current | Gain/Loss\n";
-        data.filter((d) => d.done)
-            .sort((a, b) => (a.percentGap < b.percentGap ? 1 : -1))
-            .forEach(({ currentPrice, entryPrice, percentGap, symbol }) => {
-                text += `${symbol} | $${entryPrice} | $${currentPrice} | ${percentGap.toFixed(2)}%\n`;
-            });
-        return M.reply(text);
+        return M.reply(`Cost : $${cost.toFixed(2)}`);
     };
 }
