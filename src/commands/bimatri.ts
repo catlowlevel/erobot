434b7f998e44704c1af9b5d3778cfb65d67af894
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-09-20 15:27:29 +0800

    bimatri & bimatri command

---
+++ src/commands/bimatri.ts
@@ -0,0 +1,113 @@
+import { proto } from "@adiwajshing/baileys";
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { Command } from "../core/Command";
+import { IArgs } from "../core/MessageHandler";
+import { Bimatri } from "../lib/bimatri";
+
+@Command("bimatri", {
+    description: "",
+    usage: "",
+    aliases: ["bima", "3", "tri"],
+})
+export default class extends BaseCommand {
+    getOptions(flags: string[]) {
+        const type = this.getFlag(flags, "--type", ["add-number", "get-account"]);
+        const id = this.getFlag(flags, "--id");
+        const nohp = this.getFlag(flags, "--nohp");
+        return { type, id, nohp };
+    }
+
+    public override execute = async (M: Message, { args, flags }: IArgs): Promise<any> => {
+        flags = flags.filter((f) => f.startsWith("--type") || f.startsWith("--id") || f.startsWith("--nohp"));
+        if (!M.sender.jid) throw new Error("sender jid is not defined");
+        const bima = new Bimatri();
+        await bima.db.waitInit();
+        const data = bima.db.data[M.sender.jid];
+        let numbers: string[] = [];
+        if (data) {
+            numbers = data.map((d) => d.msisdn);
+        }
+
+        const opts = this.getOptions(flags);
+        console.log("opts.id,M.sender.jid", opts.id, M.sender.jid);
+        switch (opts.type) {
+            case "add-number": {
+                if (opts.id !== M.sender.jid) return M.reply("You are not authorized perform this action");
+                await M.reply("Kirim nomor tri kamu\nEx : 6289**********");
+                const messages = await this.handler.getNewMessages(M.from, 1, 2000 * 60);
+                if (messages.length <= 0) return M.reply("Kamu tidak mengirim nomor tri kamu\nMembatalkan...");
+                const nohpMsg = messages[0];
+                const nohp = nohpMsg.content;
+                if (numbers.includes(nohp)) return M.reply("Nomor ini sudah tersimpan!");
+                try {
+                    await bima.requestOtp(nohp);
+                } catch (error) {
+                    console.log("error", error);
+                    return M.reply("Gagal meminta OTP!");
+                }
+                await nohpMsg.reply("Kirim kode OTP yang kamu terima");
+                const otpMessages = await this.handler.getNewMessages(M.from, 1, 2000 * 60);
+                if (messages.length <= 0) return M.reply("Kamu tidak mengirim kode OTP\nMembatalkan...");
+                const otpMsg = otpMessages[0];
+                const otp = otpMsg.content;
+                try {
+                    const loginData = await bima.loginOtp(nohp, otp);
+                    if (!data) bima.db.data[M.sender.jid] = [];
+                    bima.db.data[M.sender.jid].push(loginData);
+                    await bima.db.write();
+                    const account = await bima.accountData(nohp, loginData);
+                    return M.reply(JSON.stringify(account.packageList, null, 2));
+                } catch (error) {
+                    console.log("error", error);
+                    return M.reply("Gagal Login!");
+                }
+            }
+            case "get-account": {
+                if (opts.id !== M.sender.jid) return M.reply("You are not authorized perform this action");
+                if (!opts.nohp) return M.reply("No HP diperlukan!");
+                const loginData = data.find((d) => d.msisdn === opts.nohp);
+                if (!loginData) return M.reply("Tidak dapat menemukan data!");
+                const account = await bima.accountData(opts.nohp, loginData);
+                return M.reply(JSON.stringify(account.packageList, null, 2));
+            }
+
+            default: {
+            }
+        }
+
+        let text = data ? `Select Number` : "You have no saved number!";
+
+        const sections: proto.Message.ListMessage.ISection[] = [{ rows: [], title: "Saved number" }];
+        data?.forEach((d) => {
+            sections[0].rows!.push({
+                title: d.msisdn,
+                rowId: `.bimatri --type=get-account --id=${M.sender.jid} --nohp=${d.msisdn}`,
+            });
+        });
+
+        sections[0].rows!.push({
+            title: "Add Number",
+            rowId: `.bimatri --type=add-number --id=${M.sender.jid}`,
+        });
+
+        return this.client.sendMessage(
+            M.from,
+            {
+                text,
+                buttons: data
+                    ? undefined
+                    : [
+                          {
+                              buttonText: { displayText: "Add Number" },
+                              buttonId: `.bimatri --type=add-number --id=${M.sender.jid}`,
+                              type: 0,
+                          },
+                      ],
+                sections: data ? sections : undefined,
+                buttonText: "List Number",
+            },
+            { quoted: M.message }
+        );
+    };
+}
---
+++ src/lib/bimatri/index.ts
@@ -0,0 +1,91 @@
+import { ROOT_DIR } from "../..";
+import { LowDB } from "../../core/LowDB";
+import { LoginData, AccountData, OtpData } from "./types";
+
+type DBType = { [id: string]: LoginData[] };
+
+export class Bimatri {
+    db: LowDB<DBType>;
+    constructor() {
+        ////this.requestOtp(NOHP).then(() => {
+        ////console.log("done");
+        ////});
+        ////this.loginOtp(NOHP, "243802").then(() => {
+        ////console.log("DONE");
+        ////});
+        //const data = readFileSync("login.json", { encoding: "utf-8" });
+        //const loginData: LoginData = JSON.parse(data);
+        //this.accountData(NOHP, loginData).then(() => {
+        //console.log("Done ");
+        //});
+        this.db = new LowDB<DBType>(`${ROOT_DIR}/json/bimatri.json`, {});
+    }
+
+    async accountData(nohp: string, loginData: LoginData) {
+        const url = "https://bima.tri.co.id/apibima/profile/profile";
+        const { accessToken, appsflyerMsisdn, balance, creditLimit, profileColor, profileTime, status, ...rest } =
+            loginData;
+        const response = await fetch(url, {
+            headers: {
+                "content-type": "application/json",
+                Authorization: "Bearer " + loginData.accessToken,
+            },
+            //payload["imei"] = "WebSelfcare"
+            //payload["language"] = data["language"]
+            //payload["callPlan"] = data["callPlan"]
+            //payload["msisdn"] = data["msisdn"]
+            //payload["secretKey"] = data["secretKey"]
+            //payload["subscriberType"] = data["subscriberType"]
+            //payload["lastId"] = 0
+            body: JSON.stringify({
+                imei: "WebSelfcare",
+                ...rest,
+            }),
+            method: "POST",
+        });
+        if (response.status !== 200) throw new Error("Gagal mendapatkan data Akun!, " + response.statusText);
+
+        const result = (await response.json()) as AccountData;
+        return result;
+    }
+
+    async requestOtp(nohp: string) {
+        const url = "https://bima.tri.co.id/apibima/login/otp-request";
+        const response = await fetch(url, {
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                imei: "WebSelfcare",
+                msisdn: nohp,
+            }),
+            method: "POST",
+        });
+        if (response.status !== 200) throw new Error("Gagal meminta OTP!, " + response.statusText);
+
+        const result = (await response.json()) as OtpData;
+        if (!result.status) throw new Error("Terjadi kesalahan!" + " " + result.message);
+
+        return true;
+    }
+
+    async loginOtp(nohp: string, otp: string) {
+        const url = "https://bima.tri.co.id/apibima/login/login-with-otp";
+        const response = await fetch(url, {
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                imei: "WebSelfcare",
+                msisdn: nohp,
+                otp: otp,
+            }),
+            method: "POST",
+        });
+        if (response.status !== 200) throw new Error("Gagal login OTP!");
+        const result = (await response.json()) as any;
+        if (!result.status) throw new Error("Terjadi kesalahan!" + " " + result.message);
+
+        return result as LoginData;
+    }
+}
