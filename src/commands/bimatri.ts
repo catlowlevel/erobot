b91971fd19ebd24a8f0fff85f6b773a93639f28b
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-09-25 10:08:10 +0800

    update bimatri command

--- src/commands/bimatri.ts
+++ src/commands/bimatri.ts
@@ -4,6 +4,7 @@ import { BaseCommand } from "../core/BaseCommand";
 import { Command } from "../core/Command";
 import { IArgs } from "../core/MessageHandler";
 import { Bimatri } from "../lib/bimatri";
+import { LoginData } from "../lib/bimatri/types";
 
 @Command("bimatri", {
     description: "",
@@ -11,6 +12,83 @@ import { Bimatri } from "../lib/bimatri";
     aliases: ["bima", "3", "tri"],
 })
 export default class extends BaseCommand {
+    async handleGetAccount(
+        M: Message,
+        bima: Bimatri,
+        opts: { type: "add-number" | "get-account" | undefined; id: string | undefined; nohp: string | undefined }
+    ) {
+        if (!M.sender.jid) throw new Error("sender jid is not defined!");
+        if (opts.id !== M.sender.jid) return M.reply("You are not authorized perform this action");
+        if (!opts.nohp) return M.reply("No HP diperlukan!");
+
+        const data = bima.db.data[M.sender.jid];
+        const loginData = data.find((d) => d.msisdn === opts.nohp);
+        if (!loginData) return M.reply("Tidak dapat menemukan data!");
+        return this.handleAccount(M, bima, loginData);
+    }
+    async handleAccount(M: Message, bima: Bimatri, loginData: LoginData) {
+        const account = await bima.accountData(loginData);
+        const pulsa = account.balanceTotal;
+        let text = `Sisa pulsa : ${pulsa}\n`;
+        text += `Aktif sampai : ${account.validity}\n=======================\n`;
+        account.packageList.forEach((paket) => {
+            text += `${paket.name}\n`;
+            paket.detail.forEach((d) => {
+                text += `${d.value} | ${d.validity}\n`;
+            });
+            text += `=======================\n`;
+        });
+        return this.client.sendMessage(
+            M.from,
+            {
+                text,
+                buttons: [
+                    {
+                        buttonText: { displayText: "Cek lagi" },
+                        buttonId: `.bimatri --type=get-account --id=${M.sender.jid} --nohp=${loginData.msisdn}`,
+                    },
+                ],
+            },
+            { quoted: M.message }
+        );
+    }
+    async handleAddNumber(M: Message, bima: Bimatri, opts: ReturnType<typeof this.getOptions>) {
+        if (!M.sender.jid) throw new Error("sender jid is not defined!");
+        if (opts.id !== M.sender.jid) return M.reply("You are not authorized perform this action!");
+        const data = bima.db.data[M.sender.jid];
+        let numbers: string[] = [];
+        if (data) {
+            numbers = data.map((d) => d.msisdn);
+        }
+        await M.reply("Kirim nomor tri kamu\nEx : 6289**********");
+        const messages = await this.handler.getNewMessages(M, 1, 2000 * 60, true);
+        if (messages.length <= 0) return M.reply("Kamu tidak mengirim nomor tri kamu\nMembatalkan...");
+
+        const nohpMsg = messages[0];
+        const nohp = nohpMsg.content;
+        if (numbers.includes(nohp)) return M.reply("Nomor ini sudah tersimpan!");
+        try {
+            await bima.requestOtp(nohp);
+        } catch (error) {
+            console.log("error", error);
+            return M.reply("Gagal meminta OTP!");
+        }
+        await nohpMsg.reply("Kirim kode OTP yang kamu terima");
+        const otpMessages = await this.handler.getNewMessages(M, 1, 2000 * 60, true);
+        if (messages.length <= 0) return M.reply("Kamu tidak mengirim kode OTP\nMembatalkan...");
+        const otpMsg = otpMessages[0];
+        const otp = otpMsg.content;
+        try {
+            const loginData = await bima.loginOtp(nohp, otp);
+            if (!data) bima.db.data[M.sender.jid] = [];
+            bima.db.data[M.sender.jid].push(loginData);
+            await bima.db.write();
+            return this.handleAccount(M, bima, loginData);
+        } catch (error) {
+            console.log("error", error);
+            return M.reply("Gagal Login!");
+        }
+    }
     getOptions(flags: string[]) {
         const type = this.getFlag(flags, "--type", ["add-number", "get-account"]);
         const id = this.getFlag(flags, "--id");
@@ -24,61 +102,24 @@ export default class extends BaseCommand {
         const bima = new Bimatri();
         await bima.db.waitInit();
         const data = bima.db.data[M.sender.jid];
-        let numbers: string[] = [];
-        if (data) {
-            numbers = data.map((d) => d.msisdn);
-        }
 
         const opts = this.getOptions(flags);
         console.log("opts.id,M.sender.jid", opts.id, M.sender.jid);
         switch (opts.type) {
             case "add-number": {
-                if (opts.id !== M.sender.jid) return M.reply("You are not authorized perform this action");
-                await M.reply("Kirim nomor tri kamu\nEx : 6289**********");
-                const messages = await this.handler.getNewMessages(M, 1, 2000 * 60, true);
-                if (messages.length <= 0) return M.reply("Kamu tidak mengirim nomor tri kamu\nMembatalkan...");
-                const nohpMsg = messages[0];
-                const nohp = nohpMsg.content;
-                if (numbers.includes(nohp)) return M.reply("Nomor ini sudah tersimpan!");
-                try {
-                    await bima.requestOtp(nohp);
-                } catch (error) {
-                    console.log("error", error);
-                    return M.reply("Gagal meminta OTP!");
-                }
-                await nohpMsg.reply("Kirim kode OTP yang kamu terima");
-                const otpMessages = await this.handler.getNewMessages(M, 1, 2000 * 60, true);
-                if (messages.length <= 0) return M.reply("Kamu tidak mengirim kode OTP\nMembatalkan...");
-                const otpMsg = otpMessages[0];
-                const otp = otpMsg.content;
-                try {
-                    const loginData = await bima.loginOtp(nohp, otp);
-                    if (!data) bima.db.data[M.sender.jid] = [];
-                    bima.db.data[M.sender.jid].push(loginData);
-                    await bima.db.write();
-                    const account = await bima.accountData(nohp, loginData);
-                    return M.reply(JSON.stringify(account.packageList, null, 2));
-                } catch (error) {
-                    console.log("error", error);
-                    return M.reply("Gagal Login!");
-                }
+                return this.handleAddNumber(M, bima, opts);
             }
             case "get-account": {
-                if (opts.id !== M.sender.jid) return M.reply("You are not authorized perform this action");
-                if (!opts.nohp) return M.reply("No HP diperlukan!");
-                const loginData = data.find((d) => d.msisdn === opts.nohp);
-                if (!loginData) return M.reply("Tidak dapat menemukan data!");
-                const account = await bima.accountData(opts.nohp, loginData);
-                return M.reply(JSON.stringify(account.packageList, null, 2));
+                return this.handleGetAccount(M, bima, opts);
             }
 
             default: {
             }
         }
 
-        let text = data ? `Select Number` : "You have no saved number!";
+        let text = data ? `Pilih nomor yang tersimpan` : "Kamu tidak punya nomor yang tersimpan!";
 
-        const sections: proto.Message.ListMessage.ISection[] = [{ rows: [], title: "Saved number" }];
+        const sections: proto.Message.ListMessage.ISection[] = [{ rows: [], title: "Nomor tersimpan" }];
         data?.forEach((d) => {
             sections[0].rows!.push({
                 title: d.msisdn,
@@ -87,7 +128,7 @@ export default class extends BaseCommand {
         });
 
         sections[0].rows!.push({
-            title: "Add Number",
+            title: "Tambahkan nomor 3",
             rowId: `.bimatri --type=add-number --id=${M.sender.jid}`,
         });
 
@@ -99,7 +140,7 @@ export default class extends BaseCommand {
                     ? undefined
                     : [
                           {
-                              buttonText: { displayText: "Add Number" },
+                              buttonText: { displayText: "Tambahkan nomor 3" },
                               buttonId: `.bimatri --type=add-number --id=${M.sender.jid}`,
                               type: 0,
                           },
