aa9f2d1f1336dfe91a207ab9435d7c65f6dc1076
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-04 19:52:30 +0800

    debug log

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import { Message } from "../core";
 import { BaseCommand } from "../core/BaseCommand";
 import { Command } from "../core/Command";
@@ -9,10 +10,19 @@ import { IArgs } from "../core/MessageHandler";
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message, args: IArgs): Promise<any> => {
-        if (args.args[0].startsWith("process")) {
+        if (args.args.length > 0 && args.args?.[0].startsWith("process")) {
             const messages = await this.handler.getNewMessages(M, 5, 1000 * 15, true);
             messages.forEach((m) => m.replyQueue(m.content));
             return;
+        }
+        if (args.args.length > 0 && args.args[0].startsWith("log")) {
+            return new Promise<void>((res, rej) => {
+                exec("pm2 logs erobot --raw --nostream", async (err, out) => {
+                    if (err) rej(err);
+                    await M.reply(out);
+                    res();
+                });
+            });
         }
         if (M.quoted) {
             console.log(JSON.stringify(M.quoted, null, 2));
