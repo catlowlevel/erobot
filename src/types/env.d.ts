63f2ae6dd5a18cac4e826e76769b645f43d784ef
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-07 20:10:00 +0800

    update env example

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,6 +5,9 @@ declare global {
             COINDAR_BASE_URL_API: string;
             BITLY_ACCESS_TOKEN: string;
             BITLY_BASE_URL: string;
+            BINANCE_APIKEY: string;
+            BINANCE_APISECRET: string;
+            REMOVEBG_APIKEY: string;
         }
     }
 }
