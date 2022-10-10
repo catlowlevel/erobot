7a5c256258c4c7f06df6f290a3c19156a23e0c3e
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-10 14:26:30 +0800

    apakah command

---
+++ src/commands/apakah.ts
@@ -0,0 +1,17 @@
+import { Message } from "../core";
+import { BaseCommand } from "../core/BaseCommand";
+import { Command } from "../core/Command";
+import { IArgs } from "../core/MessageHandler";
+
+@Command("apakah", {
+    description: "",
+    usage: "",
+})
+export default class extends BaseCommand {
+    public override execute = async (M: Message, args: IArgs): Promise<any> => {
+        if (args.args.length <= 0) return M.reply("apaan?");
+        const answers = ["iya", "tidak", "mungkin iya", "mungkin tidak", "tentu iya", "tentu tidak"];
+        const answer = answers[Math.floor(Math.random() * answers.length)];
+        return M.reply(answer);
+    };
+}
