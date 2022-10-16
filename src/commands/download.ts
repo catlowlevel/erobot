c70184ba2a258c00f741794434a8863c624d1343
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-16 08:32:04 +0800

    downloader

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -1,8 +1,6 @@
 import getUrls from "get-urls";
-import fetch from "node-fetch-commonjs";
-import convert from "../../node_modules/convert-pro/convert";
 import { BaseCommand, Command, IArgs, Message } from "../core";
-import { generateRandomUniqueTag } from "../helper/utils";
+import { downloadFile } from "../lib/downloader";
 
 @Command("download", {
     description: "",
@@ -14,30 +12,21 @@ export default class extends BaseCommand {
         const urls = getUrls(args.context);
         if (!urls) return M.reply("Url required!");
         const url = urls.values().next().value as string;
-        const response = await fetch(url);
-        const contentLength = response.headers.get("content-length");
-        if (!contentLength) return M.reply("This url is possibly not downloadable!");
-        const contentDisposition = response.headers.get("content-disposition");
-        const parts = contentDisposition?.split(";");
-        let fileName = parts?.[1].split("=")[1];
-        if (!fileName) fileName = url.substring(url.lastIndexOf("/") + 1);
-        if (!fileName) fileName = generateRandomUniqueTag(10);
-        console.log("filename :>> ", fileName);
+        const { buffer, mimeType, fileName } = await downloadFile(url, async ({ fileName, size, sizeStr }) => {
+            if (size <= 0) {
+                M.reply("This url is possibly not downloadable!");
+                return false;
+            }
+            if (size <= 0.5) {
+                M.reply(`Minimal size 500 KB\nFile size : *${sizeStr}*`);
+                return false;
+            }
 
-        //TODO: Make this into function
-        const size = convert.bytes(Number(contentLength), "MB");
-        const sizeStr = convert.bytes(Number(contentLength), { accuracy: 2 });
-        if (size >= 100) return M.reply(`Max file size is 100MB\nFile size : ${sizeStr}`);
-        M.reply(`Downloading *${fileName}*\nSize : ${sizeStr}`);
-        console.log(`Downloading ${fileName}...`);
-
-        const blob = await response.blob();
-        console.log("Download done");
-        const mimeType = blob.type;
-        console.log("mimeType :>> ", mimeType);
-        console.log("Converting to buffer...");
-        const buffer = Buffer.from(await blob.arrayBuffer());
-        console.log("Convert done");
-        return M.reply(buffer, "document", undefined, mimeType, undefined, undefined, undefined, undefined, fileName);
+            await M.reply(`Downloading... *${fileName}*\nSize : *${sizeStr}*`);
+            return true;
+        });
+        if (!buffer) return;
+        await M.react("Download Completed\nUploading...");
+        M.reply(buffer, "document", undefined, mimeType, undefined, undefined, undefined, undefined, fileName);
     };
 }
---
+++ src/lib/downloader/index.ts
@@ -0,0 +1,38 @@
+import convert from "../../../node_modules/convert-pro/convert";
+import { generateRandomUniqueTag } from "../../helper/utils";
+
+export const downloadFile = async (
+    url: string,
+    confirmation?: (detail: { fileName: string; size: number; sizeStr: string }) => boolean | Promise<boolean>
+) => {
+    const response = await fetch(url);
+    const contentLength = response.headers.get("content-length") ?? 0;
+    console.log("contentLength :>> ", contentLength);
+    // if (!contentLength) throw new Error(`This url is possibly not downloadable!\n${JSON.stringify(response)}`);
+    const contentDisposition = response.headers.get("content-disposition");
+    const parts = contentDisposition?.split(";");
+    let fileName = parts?.[1].split("=")[1];
+    if (!fileName) fileName = url.substring(url.lastIndexOf("/") + 1);
+    if (!fileName) fileName = generateRandomUniqueTag(10);
+    console.log("filename :>> ", fileName);
+
+    const size = convert.bytes(Number(contentLength), "MB");
+
+    const sizeStr = convert.bytes(Number(contentLength), { accuracy: 2 });
+
+    if (confirmation) {
+        const result = await confirmation({ fileName, size, sizeStr });
+        if (!result) return { fileName: "", mimeType: "", buffer: null };
+    }
+
+    console.log(`Downloading ${fileName}...`);
+
+    const blob = await response.blob();
+    console.log("Download done");
+    const mimeType = blob.type;
+    console.log("mimeType :>> ", mimeType);
+    console.log("Converting to buffer...");
+    const buffer = Buffer.from(await blob.arrayBuffer());
+    console.log("Convert done");
+    return { buffer, mimeType, fileName };
+};
