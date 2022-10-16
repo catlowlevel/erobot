1e1075fd344612d85c2520aa2a362cdeb1a7437b
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-16 08:32:54 +0800

    add MONGO_URI env variable

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,15 +1,16 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            COINDAR_ACCESSTOKEN: string;
-            COINDAR_BASE_URL_API: string;
-            BITLY_ACCESS_TOKEN: string;
-            BITLY_BASE_URL: string;
-            BINANCE_APIKEY: string;
-            BINANCE_APISECRET: string;
-            REMOVEBG_APIKEY: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      COINDAR_ACCESSTOKEN: string;
+      COINDAR_BASE_URL_API: string;
+      BITLY_ACCESS_TOKEN: string;
+      BITLY_BASE_URL: string;
+      BINANCE_APIKEY: string;
+      BINANCE_APISECRET: string;
+      REMOVEBG_APIKEY: string;
+      MONGO_URI: string;
     }
+  }
 }
 
-export {};
+export {}
