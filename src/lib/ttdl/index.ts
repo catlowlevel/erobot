9c9b9db41b7aafa13cdbf08c5c1001002d83c0d1
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-10-31 05:20:11 +0000

    add ttdl lib

---
+++ src/lib/ttdl/index.ts
@@ -0,0 +1,19 @@
+import fetch from "node-fetch-commonjs";
+
+interface Result {
+    video?: Video;
+    provider: string;
+}
+interface ResultError {
+    error?: string;
+    provider: string;
+}
+
+interface Video {
+    thumb: string;
+    urls: string[];
+}
+
+type TikTokDL = Result & ResultError;
+export const tiktokDl = async (url: string) =>
+    fetch(`http://localhost:3123/api/download?url=${url}`).then((r) => r.json() as Promise<TikTokDL>); //https://github.com/hansputera/tiktok-dl
