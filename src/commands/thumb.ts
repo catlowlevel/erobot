1a19908f24902467a951e06a84ae86bb4f26a72a
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-11-22 03:40:25 +0000

    only re-set buffer if buffer is undefined

--- src/commands/thumb.ts
+++ src/commands/thumb.ts
@@ -9,7 +9,7 @@ export default class extends BaseCommand {
     public override execute = async (M: Message, _args: IArgs): Promise<unknown> => {
         let buffer = await M.downloadQuotedMedia();
         if (!buffer && M.type !== "imageMessage") return M.reply("Not an image!");
-        buffer = (await downloadMediaMessage(M.message, "buffer", {})) as Buffer;
+        if (!buffer) buffer = (await downloadMediaMessage(M.message, "buffer", {})) as Buffer;
         const result = await this.client.utils.yandexThumbnail(buffer);
         const imageBuffer = await this.client.utils.getBuffer(result.url);
 
