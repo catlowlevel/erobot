43a14085792d63bd0c019dc7b120b240af167de0
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-12-01 07:06:23 +0000

    add limit

--- src/commands/sauce.ts
+++ src/commands/sauce.ts
@@ -13,9 +13,22 @@ export default class extends BaseCommand {
         let buffer = await M.downloadQuotedMedia();
         if (!buffer && M.type !== "imageMessage") return M.reply("Not an image!");
         if (!buffer) buffer = (await downloadMediaMessage(M.message, "buffer", {})) as Buffer;
+        let limit = 10;
+        args.args.forEach((a) => {
+            const num = Number(a);
+            if (!isNaN(num) && num > 0) {
+                limit = num;
+            } else {
+                console.log(`${a} is ${num}`);
+            }
+        });
+        M.reply("_Searching..._");
         const yandexResult = await this.client.utils.yandexThumbnail(buffer);
         const result = await searchImages(yandexResult);
-        for (const r of result.slice(0, 10)) {
+        console.log("limit", limit);
+        console.log("result.length", result.length);
+
+        for (const r of result.slice(0, limit)) {
             console.log(`sending : ${r.title}`);
             const image = await this.client.utils.getBuffer(r.thumb);
             const link = await shortenUrl(r.link);
