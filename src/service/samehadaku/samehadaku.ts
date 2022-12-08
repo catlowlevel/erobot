f4a6b692e3f440f82f0dc14a4c882f9e52dc8d28
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-12-08 08:07:45 +0000

    fixme

--- src/service/samehadaku/samehadaku.ts
+++ src/service/samehadaku/samehadaku.ts
@@ -140,7 +140,23 @@ export class Samehadaku {
 
     async getPosts() {
         //FIXME: bypass cloudflare by providing extra headers here
-        const response = await fetch(this.BASE_URL);
+        //FIXME: additional headers does not fix this issue
+        const response = await fetch(this.BASE_URL, {
+            credentials: "include",
+            headers: {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Alt-Used": "samehadaku.win",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+            },
+            method: "GET",
+            mode: "cors",
+        });
         const html = await response.text();
         const $ = cheerio.load(html);
         const postShow = $("div.post-show").first();
