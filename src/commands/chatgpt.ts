0ab23c185dd8a6975de8aa6aedeed4555cfd97ba
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-02-13 07:11:42 +0000

    update chatgpt

--- src/commands/chatgpt.ts
+++ src/commands/chatgpt.ts
@@ -1,4 +1,4 @@
-import { ChatGPTAPI } from "chatgpt";
+import { ChatGPTAPI, ChatMessage } from "chatgpt";
 import { BaseCommand, Command, IArgs, Message } from "../core";
 
 @Command("chatgpt", {
@@ -10,11 +10,12 @@ export default class extends BaseCommand {
     public override execute = async (M: Message, args: IArgs): Promise<unknown> => {
         if (!process.env.OPENAI_API_KEY) throw new Error("NO OPENAI APIKEY!");
         const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY as string });
-        const result = await api.sendMessage(args.context);
-        return M.reply(result.text);
+        this.lastResult = await api.sendMessage(args.context, { parentMessageId: this.lastResult?.id });
+        return M.reply(this.lastResult.text);
     };
 
     public handleError = async (M: Message, err: Error): Promise<unknown> => {
         return M.reply(err.message);
     };
+    lastResult: ChatMessage;
 }
