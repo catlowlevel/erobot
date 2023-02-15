bb6b451ddcc494531c5c7bed28d7cd3ecf55bb83
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-02-15 13:21:39 +0000

    refactor eval (need improvement)

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -16,6 +16,25 @@ export default class extends BaseCommand {
         return new Function("with(this) { return eval('" + scr + "'); }").call(context);
     }
     public override execute = async (M: Message, { context }: IArgs): Promise<unknown> => {
+        const lines = context.split("\n");
+        lines[lines.length - 1] = "return " + lines[lines.length - 1];
+        const parsed = lines.join("\n");
+        const evaluate = await eval(`;(async () => { ${parsed} })()`).catch((e: Error) => {
+            return e.message;
+        });
+        console.log(evaluate);
+        return M.reply(
+            `${JSON.stringify(
+                evaluate,
+                (key, value) => {
+                    if (typeof value === "function") {
+                        return (value as string).toString().split("\n")[0];
+                    }
+                    return value;
+                },
+                "\t"
+            )}`
+        );
         let out!: string;
         try {
             const obj = {
@@ -26,10 +45,15 @@ export default class extends BaseCommand {
                 ...this,
             };
             const result = this.evalInContext(context, obj);
+            console.log(result);
             out = JSON.stringify(result, null, "\t") || "Evaluated JavaScript";
         } catch (error) {
             out = (error as Error).message;
         }
         return M.reply(out);
+    };
+
+    public override handleError = async (M: Message, err: Error): Promise<unknown> => {
+        return M.reply(err.message);
     };
 }
