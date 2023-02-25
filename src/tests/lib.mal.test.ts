4cdcb975fcf298087feb6ee3a0017ef925bd1729
mIsPtr <72902682+mIsPtr@users.noreply.github.com>
2023-02-25 11:48:18 +0800

    update test value

--- src/tests/lib.mal.test.ts
+++ src/tests/lib.mal.test.ts
@@ -2,5 +2,5 @@ import { searchAnime } from "../lib/mal";
 
 test("return an array of anime", async () => {
     const anime = await searchAnime("naruto");
-    expect(anime.data[0].mal_id).toBe(53240);
+    expect(anime?.data?.length).toBeGreaterThan(0);
 });
